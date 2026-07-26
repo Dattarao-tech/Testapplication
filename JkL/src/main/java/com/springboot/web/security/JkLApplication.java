ae5ddc1e8d34e079456e5b3d0bdc34b05c313164
dattaraodeosarkar4@gmail.com <dattaraodeosarkar4@gmail.com>
2026-07-26 22:01:01 +0530

    Initial commit

--- JkL/src/main/java/com/springboot/web/security/JkLApplication.java
+++ JkL/src/main/java/com/springboot/web/security/JkLApplication.java
@@ -1,11 +1,16 @@
 package com.springboot.web.security;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
 
 @SpringBootApplication
 public class JkLApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JkLApplication.class, args);	
 	}
+
+	
+
 }
